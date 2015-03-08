4d8cd5ea0ed146b238f906881506aba5866435c7
iproduct <office@iproduct.org>
2015-03-08 11:59:32 +0200

    bootstrap project initial commit

--- HTML5_Bootstrap/public_html/js/custom.js
+++ HTML5_Bootstrap/public_html/js/custom.js
@@ -2,7 +2,7 @@
  * Bootstrap and jQuery demo
  */
 
-function init() {
-    document.getElementById("output").innerHTML = "Hello from JavaScript!";
-}
+//function init() {
+//    document.getElementById("output").innerHTML = "Hello from JavaScript!";
+//}
 
